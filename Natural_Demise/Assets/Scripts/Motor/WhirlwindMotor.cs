ad62ed50681c10d74f9ef2748d4a82e632d18392
Engstrom <isaen688@student.liu.se>
2020-08-06 16:51:50 +0200

    Add skeleton code for Whirlwind motor and whirl state.

---
+++ Natural_Demise/Assets/Scripts/Motor/WhirlwindMotor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhirlwindMotor : BaseMotor {
+    //TODO: Implement the properties of the whirlwind 
+    
+    protected override void UpdateMotor() {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Construct() {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Natural_Demise/Assets/Scripts/State/WhirlState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhirlState : BaseState {
+    //TODO: Implement the behaviour of the WhirlState
+    
+    public override Vector3 ProcessMotion(Vector3 input) {
+        throw new System.NotImplementedException();
+    }
+
+  
+}
