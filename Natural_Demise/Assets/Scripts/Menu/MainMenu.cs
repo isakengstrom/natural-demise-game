42293efd2808d52c46cba932997a213a78fc4a66
Engstrom <isaen688@student.liu.se>
2020-08-12 23:55:36 +0200

    Add scripts for menues.

---
+++ Natural_Demise/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Following the tutorial provided by Brackeys on Youtube. 
+//Source: https://www.youtube.com/watch?v=zc8ac_qUXQY
+public class MainMenu : MonoBehaviour {
+   public void PlayGame() {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void QuitGame() {
+      print("Quitting game..");
+      Application.Quit();
+   }
+    
+}
---
+++ Natural_Demise/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Following the tutorial provided by Brackeys on Youtube. 
+//Source: https://www.youtube.com/watch?v=JivuXdrIHK0 
+public class PauseMenu : MonoBehaviour {
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isGamePaused) Resume();
+            else _pause();
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    private void _pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame() {
+        print("Quitting game..");
+        Application.Quit();
+    }
+}
